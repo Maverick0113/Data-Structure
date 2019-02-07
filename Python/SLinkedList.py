3475dc7a48c18b23b89083cae50cfca1b9d23644
Maverick0113 <40501876+Maverick0113@users.noreply.github.com>
2019-02-07 11:39:32 -0800

    Add files via upload

---
+++ Python/SLinkedList.py
@@ -0,0 +1,149 @@
+class Node:
+    def __init__(self, data):
+        self.val = data
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Add an item to the end of the Linked List
+    def append(self, x):
+        # if the Linked List is empty
+        if not self.head:
+            self.head = Node(x)
+            return
+
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = Node(x)
+
+    # Extend the Linked List by appending all the items in the array
+    def extend(self, arr):
+        # if the Linked List is empty
+        if not self.head:
+            self.head = Node(arr[0])
+            cur = self.head
+            for i in range(1, len(arr)):
+                new = Node(arr[i])
+                cur.next = new
+                cur = cur.next
+            return
+
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        for i in arr:
+            new = Node(i)
+            cur.next = new
+            cur = cur.next
+
+    # Insert an item at a given position
+    def insert(self, i, x):
+        new = Node(x)
+        # if the position [i] is negative
+        if i < 0:
+            print("Out of bound index")
+            return
+
+        # if inserting at the first position
+        if i == 0:
+            new.next = self.head
+            return
+
+        index = int(0)
+        cur = self.head
+        # Continue traversing as long as index is less than
+        # position [i - 1] and current node's [next] exist
+        while index < i - 1 and cur.next:
+            cur = cur.next
+            index += 1
+
+        # if the inserting at the last position
+        if not cur.next:
+            cur.next = new
+            return
+
+        new.next = cur.next.next
+        cur.next = new
+    
+    # remove the first item from the list whose value is equal to [x]
+    def remove(self, x):
+        # if the removed item is at first position
+        if self.head.val == x:
+            self.head = self.head.next
+            return
+
+        cur = self.head
+        prev = None
+        found = False
+        while cur:
+            if cur.val == x:
+                found = True
+                break
+            prev = cur
+            cur = cur.next
+
+        # if item does not exist in Linked List
+        if not found:
+            print("Item not found")
+            return
+
+        # if the removed item is at last position
+        if not cur.next:
+            prev.next = None
+            return
+
+        prev.next = cur.next
+    
+    # remove the item at the given position in the list, and return it
+    def pop(self, i):
+
+        # if the position [i] is negative
+        if i < 0:
+            print("Out of bound index")
+            return
+
+        # if the popped item is at first position
+        if i == 0:
+            self.head= self.head.next
+            return self.head.val
+
+        cur = self.head
+        prev = None
+        index = int(0)
+        # Continue traversing as long as index is less than
+        # position [i] and current node's [next] exist
+        while index < i and cur.next:
+            prev = cur
+            cur = cur.next
+            index += 1
+
+        # if the removed item is at last position
+        if not cur.next:
+            prev.next = None
+            return cur.val
+
+        prev.next = cur.next
+        return cur.val
+
+    # print current linked list as a list
+    def print(self):
+        if not self.head:
+            print("Empty Linked List")
+            return
+
+        cur = self.head
+        arr = []
+        while cur:
+            arr.append(cur.val)
+            cur = cur.next
+        print(arr)
+        
+
+l = SLinkedList()
+l.extend([3, 4, 5])
+print(l.pop(3))
+print(l.pop(2))
+l.print()
