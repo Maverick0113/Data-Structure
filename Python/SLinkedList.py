a378d71e2106d833be8aa04cab1b72b50c8d6a16
Maverick0113 <40501876+Maverick0113@users.noreply.github.com>
2019-02-08 14:45:47 -0800

    Add files via upload

--- Python/SLinkedList.py
+++ Python/SLinkedList.py
@@ -1,13 +1,16 @@
 class Node:
+    # Single Linked List Node constructor
     def __init__(self, data):
         self.val = data
         self.next = None
 
 class SLinkedList:
+    # Single Linked List constructor
     def __init__(self):
         self.head = None
 
-    # Add an item to the end of the Linked List
+    # append(x)
+    # Add an item[x] to the end of the Linked List
     def append(self, x):
         # if the Linked List is empty
         if not self.head:
@@ -18,7 +21,8 @@ class SLinkedList:
         while cur.next:
             cur = cur.next
         cur.next = Node(x)
-
+    
+    # extend(iterable)
     # Extend the Linked List by appending all the items in the array
     def extend(self, arr):
         # if the Linked List is empty
@@ -39,7 +43,8 @@ class SLinkedList:
             cur.next = new
             cur = cur.next
 
-    # Insert an item at a given position
+    # insert(i, x)
+    # Insert an item[x] at a given position
     def insert(self, i, x):
         new = Node(x)
         # if the position [i] is negative
@@ -67,7 +72,8 @@ class SLinkedList:
 
         new.next = cur.next.next
         cur.next = new
-    
+
+    # remove(x)   
     # remove the first item from the list whose value is equal to [x]
     def remove(self, x):
         # if the removed item is at first position
@@ -96,8 +102,11 @@ class SLinkedList:
             return
 
         prev.next = cur.next
-    
+
+    # pop([i])
     # remove the item at the given position in the list, and return it
+    # if no index is specified, remove and
+    # return the last item in the list
     def pop(self, i):
 
         # if the position [i] is negative
@@ -128,6 +137,78 @@ class SLinkedList:
         prev.next = cur.next
         return cur.val
 
+    # clear()
+    # remove all items from the list
+    def clear(self):
+        self.head = None
+
+    # index(x)
+    # return zero-based index in the list of the first item
+    # whose value is equal to [x]
+    def index(self, x):
+        i = int(0)
+        cur = self.head
+        while cur:
+            if cur.val == x:
+                return i
+            i += 1
+            cur = cur.next
+        return -1
+
+    # count(x)
+    # return the number of times [x] appears in the list
+    def count(self, x):
+        cnt = int(0)
+        cur = self.head
+        while cur:
+            if cur.val == x:
+                cnt += 1
+            cur = cur.next
+        return cnt
+
+    # sort(reverse = False)
+    # sort the items of the list in place
+    def sort(self, flag):
+        arr = []
+        cur = self.head
+        while cur:
+            arr.append(cur.val)
+            cur = cur.next
+
+        arr.sort(reverse = not flag)
+
+        self.head = Node(arr[0])
+        cur = self.head
+        for i in range(1, len(arr)):
+            cur.next = Node(arr[i])
+            cur = cur.next
+
+    # reverse()
+    # reverse the elements of the list in place
+    def reverse(self):
+        cur = self.head
+        prev = None
+        nex = cur.next
+        while nex:
+            cur.next = prev
+            prev = cur
+            cur = nex
+            nex = nex.next
+        cur.next = prev
+        self.head = cur
+    
+    # copy()
+    # return a shallow copy of the list
+    def copy(self):
+        if not self.head:
+            return None
+        new = SLinkedList()
+        cur = self.head
+        while cur:
+            new.append(cur.val)
+            cur = cur.next
+        return new
+
     # print current linked list as a list
     def print(self):
         if not self.head:
@@ -143,7 +224,6 @@ class SLinkedList:
         
 
 l = SLinkedList()
-l.extend([3, 4, 5])
-print(l.pop(3))
-print(l.pop(2))
+l.extend([2, 3, 1])
+l.sort(True)
 l.print()
